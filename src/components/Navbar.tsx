22cec96a09f460c03f7c275e775b4bd1b544ed11
illiakos <kostrubailla@gmail.com>
2023-08-18 15:40:37 +0300

    Navbar implemented

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image'
+import {useRouter} from "next/router";
+import { MdAlternateEmail } from 'react-icons/md';
+
+export default function Navbar() {
+
+    const  router = useRouter();
+
+
+    return (
+        <header className={"flex w-full h-fit mx-16 mt-8 justify-between font-primary"}>
+                <div className={"relative w-64"}>
+                    <Image
+                        draggable={false}
+                        onClick={() => {
+                            router.push('/')
+                        }}
+                        layout={'fill'}
+                        objectFit={'contain'}
+                        src={'/images/navbar/logo.svg'}
+                        alt={"Logo"}
+                        className={"cursor-pointer outline-0 remove-highlight z-[115]"}
+                    />
+                </div>
+                <ul className={"gap-x-8 flex text-text-black text-lg font-medium w-fit"}>
+                    <li className={"cursor-pointer hover:underline "}>
+                        About us
+                    </li>
+                    <li className={"cursor-pointer hover:underline "}>
+                        Services
+                    </li>
+                    <li className={"cursor-pointer hover:underline "}>
+                        Projects
+                    </li>
+                    <li className={"cursor-pointer hover:underline "}>
+                        Steps
+                    </li>
+                </ul>
+            <ul className={"flex text-text-black text-lg font-medium w-fit"}>
+                <MdAlternateEmail className={"mt-1.5 mr-1 hover:scale-110 transitional-all duration-300 cursor-pointer"}/>
+                <li className={"cursor-pointer hover:underline font-medium"}>white.soft.business@gmail.com</li>
+            </ul>
+        </header>
+    )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,30 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary-blue': '#23324D',
+      'primary-light-blue': '#E1EAF7',
+      'primary-grey': '#F6F8FB',
+      'white': '#FFFFFF',
+      'text-black': '#1B1B1B',
+      'text-grey': '#686D78',
+      'blue-1': '#BED8FF',
+      'blue-2': '#B4CDF2',
+      'blue-3': '#B6C9E3',
+      'blue-4': '#9BB1D1',
+      'blue-5': '#97A4BB',
+      'blue-6': '#636B7A',
+      'blue-7': '#192438',
+      'blue-8': '#151E2E',
+      'error':'#F97364',
+      'black-rgba':'rgba(0,0,0,0.90)'
+    },
     extend: {
+      fontFamily:{
+        'primary': ['Montserrat']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
@@ -14,5 +37,8 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+      require("@tailwindcss/typography"),
+      require("daisyui")
+  ],
 }
